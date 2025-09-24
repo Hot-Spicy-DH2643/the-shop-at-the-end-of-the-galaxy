00ab32d2984409d5b85501f4323e72d11ab0b907
Lambozhuang <Lambozhuang@gmail.com>
2025-09-24 22:33:00 +0200

    Configure backend to be pure API service, remove views

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET API root */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.json({ 
+    message: 'Welcome to The Shop at the End of the Galaxy API',
+    version: '1.0.0',
+    endpoints: {
+      users: '/api/users'
+    }
+  });
 });
 
 module.exports = router;
