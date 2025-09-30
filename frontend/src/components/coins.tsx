13843d16c2f2be1ec166e1052791db9354e1489e
Maja Lind <maja01.lind@hotmail.com>
2025-09-30 11:56:31 +0200

    added components and functionallity to profile page

---
+++ frontend/src/components/coins.tsx
@@ -0,0 +1,8 @@
+export default function Coins() {
+  return (
+    <div>
+      <h2 className="text-xl font-bold text-white">Coins</h2>
+      <p className="text-white mt-2">Coins is here</p>
+    </div>
+  );
+}
