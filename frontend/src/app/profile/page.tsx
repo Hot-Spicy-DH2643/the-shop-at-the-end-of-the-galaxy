2365bb4b91cd30d7e6cdc22d568f36abcf43efe3
Lucky Hyosun 효선킴 <73243458+HellobonjourLucky@users.noreply.github.com>
2025-10-03 10:32:35 +0200

    Update user image

--- frontend/src/app/profile/page.tsx
+++ frontend/src/app/profile/page.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import Navbar from '@/components/navbar';
 import ProfileTab from './ProfileTabs';
 import { getUser, UserType } from './users';
@@ -19,11 +18,12 @@ export default async function Profile() {
       <div className="flex flex-col lg:flex-row flex-wrap justify-center items-center lg:items-start px-8 mt-4 md:mt-20 md:gap-8 lg:gap-12">
         {/* Profile Image */}
         <div className="flex flex-row lg:flex-col justify-center items-center gap-4">
-          <div className="w-20 h-20 md:w-40 md:h-40 rounded-full bg-gray-400"></div>
-          {/* Replace with your image:
-          <img src="/profile.jpg" alt="Profile"
-            className="w-32 h-32 rounded-full object-contain border-2 border-gray-300"
-          /> */}
+          {/* <div className="w-20 h-20 md:w-40 md:h-40"></div> */}
+          <img
+            src="/default-user-img.png"
+            alt="Profile"
+            className="w-20 h-20 md:w-40 md:h-40 rounded-full object-contain"
+          />
           <div className="text-white text-center leading-8">
             {/* user information */}
             <h4 className="text-2xl md:text-4xl lg:text-3xl xl:text-4xl font-semibold">
