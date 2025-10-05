d3016e30c1583b13fb93ad7cee024103ecfbd3f5
Lucky Hyosun 효선킴 <73243458+HellobonjourLucky@users.noreply.github.com>
2025-10-05 16:46:56 +0200

    Add client-side navigation (reading the query string from url and navigate)

--- frontend/src/app/profile/ProfileTabs.tsx
+++ frontend/src/app/profile/ProfileTabs.tsx
@@ -1,11 +1,13 @@
 'use client';
-import { useState } from 'react';
-import { UserType } from './users';
+import { useState, useEffect } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
 
+import { UserType } from './users';
 import User from './ProfileComponents/User';
 import Galaxy from './ProfileComponents/Galaxy';
 import Purchases from './ProfileComponents/Purchases';
 import Friends from './ProfileComponents/Friends';
+
 interface ProfileTabProps {
   user: UserType;
 }
@@ -13,22 +15,36 @@ interface ProfileTabProps {
 type Tab = 'user' | 'purchases' | 'galaxy' | 'friends';
 
 const ProfileTab = ({ user }: ProfileTabProps) => {
-  const [activeTab, setActiveTab] = useState<Tab>('user');
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const currentTab = (searchParams.get('tab') as Tab) || 'user';
+  const [activeTab, setActiveTab] = useState<Tab>(currentTab);
+
+  useEffect(() => {
+    setActiveTab(currentTab);
+  }, [currentTab]);
+
+  const handleNavigation = (tab: Tab) => {
+    setActiveTab(tab);
+    const newUrl = `/profile?tab=${tab}`;
+    router.push(newUrl);
+  };
 
   return (
     <div className="flex flex-col mt-10 md:mt-0">
+      {/* Tab Buttons */}
       <div className="grid grid-cols-2 sm:grid-cols-4 gap-4 mb-6">
         {(['user', 'purchases', 'galaxy', 'friends'] as Tab[]).map(tab => (
           <button
             key={tab}
-            onClick={() => setActiveTab(tab)}
+            onClick={() => handleNavigation(tab)}
             className={`px-4 py-3 text-sm sm:px-6 sm:py-2 sm:text-base font-bold rounded shadow cursor-pointer ${
               activeTab === tab
                 ? 'bg-gray-300'
                 : 'bg-gray-200 hover:bg-gray-300'
             }`}
           >
-            {tab.toUpperCase().replace('_', ' ')}
+            {tab.toUpperCase()}
           </button>
         ))}
       </div>
