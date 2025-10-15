8856e1b08f3882c2c6cf36361505848a5517a5b8
Lucky Hyosun 효선킴 <73243458+HellobonjourLucky@users.noreply.github.com>
2025-10-15 11:21:41 +0200

    Change asteroids type from the db and graphql

--- backend/src/services/userService.js
+++ backend/src/services/userService.js
@@ -1,5 +1,6 @@
 import { User } from '../models/User.js';
 import { Asteroid } from '../models/Asteroid.js';
+import { getAsteroidById } from './externalApiService.js';
 
 export async function getAllUsers() {
   try {
@@ -19,40 +20,31 @@ export async function getUserById(userId) {
       return null;
     }
 
-    const owned_asteroids = await Asteroid.find(
-      { id: { $in: user.owned_asteroids } },
-      {
-        id: 1,
-        name: 1,
-        is_potentially_hazardous_asteroid: 1,
-        price: 1,
-        size: 1,
-        _id: 0,
-      }
+    const owned_asteroid_ids = user.owned_asteroid_ids;
+    const owned_asteroids = await Promise.all(
+      owned_asteroid_ids.map(id => getAsteroidById(id))
     );
 
-    const starred_asteroids = await Asteroid.find(
-      { id: { $in: user.starred_asteroids } },
-      {
-        id: 1,
-        name: 1,
-        is_potentially_hazardous_asteroid: 1,
-        price: 1,
-        size: 1,
-        _id: 0,
-      }
+    const starred_asteroid_ids = user.starred_asteroid_ids;
+    const starred_asteroids = await Promise.all(
+      starred_asteroid_ids.map(id => getAsteroidById(id))
     );
 
     // Fetch followers' details
     const followers = await User.find(
-      { uid: { $in: user.followers } },
+      { uid: { $in: user.follower_ids } },
       { uid: 1, name: 1, _id: 0 }
     );
 
     // Fetch following users' details
     const following = await User.find(
-      { uid: { $in: user.following } },
+      { uid: { $in: user.following_ids } },
       { uid: 1, name: 1, _id: 0 }
+    );
+
+    const cart_asteroid_ids = user.cart_asteroid_ids;
+    const cart_asteroids = await Promise.all(
+      cart_asteroid_ids.map(id => getAsteroidById(id))
     );
 
     // Transform the user object to include followers and following
@@ -60,6 +52,7 @@ export async function getUserById(userId) {
       ...user.toObject(),
       starred_asteroids,
       owned_asteroids,
+      cart_asteroids,
       followers,
       following,
     };
